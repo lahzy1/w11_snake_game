ed4c66a4ba13bdc8197814df1360bfb6b38ff8bc
lahzy-1 <larpre01@easv365.dk>
2023-03-22 15:56:27 +0100

    Update HelloApplication.java

--- src/main/java/com/example/w11_snake_game/HelloApplication.java
+++ src/main/java/com/example/w11_snake_game/HelloApplication.java
@@ -2,7 +2,14 @@ package com.example.w11_snake_game;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -10,9 +17,37 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        //FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+
+        // Game Screen
+        Pane gamePane = new Pane();
+
+        // Main Menu
+        Label titleLabel = new Label("SNAKE");
+        titleLabel.setPadding(new Insets(80));
+
+        Button resumeButton = new Button("Resume Game");
+        resumeButton.setDisable(true);
+
+        Button newGameButton = new Button("New Game");
+
+        Button insaneModeButton = new Button("New Game (Insane)");
+
+        Button exitButton = new Button("Exit");
+
+        VBox menuVBox = new VBox(titleLabel, resumeButton, newGameButton, insaneModeButton, exitButton);
+        menuVBox.setAlignment(Pos.CENTER);
+        menuVBox.setSpacing(20);
+
+        BorderPane menuBorderPane = new BorderPane();
+        menuBorderPane.setCenter(menuVBox);
+
+
+
+        //
+        Scene scene = new Scene(menuBorderPane, 800, 800);
+
+        stage.setTitle("Sssssnake");
         stage.setScene(scene);
         stage.show();
     }
