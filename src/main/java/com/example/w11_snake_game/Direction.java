6f49eebda9aec7225900b13534ca08b18331fb1b
lahzy-1 <larpre01@easv365.dk>
2023-03-26 16:11:27 +0200

    1611

---
+++ src/main/java/com/example/w11_snake_game/Direction.java
@@ -0,0 +1,5 @@
+package com.example.w11_snake_game;
+
+public enum Direction {
+    UP, DOWN, LEFT, RIGHT
+}
--- src/main/java/com/example/w11_snake_game/Snake.java
+++ src/main/java/com/example/w11_snake_game/Snake.java
@@ -3,11 +3,10 @@ package com.example.w11_snake_game;
 import javafx.scene.paint.Color;
 
 public class Snake {
-    private enum Direction {UP, DOWN, LEFT, RIGHT}
-
-    private int xSnakePosition = 300;
-    private int ySnakePosition = 300;
+    private int xSnakeHeadPosition = 300;
+    private int ySnakeHeadPosition = 300;
     private int snakeSize = 3;
+    private int snakeHeadSize = 1;
     private int snakeSpeed = 4; // 4 squares per second?
     private Color snakeColor = Color.WHITESMOKE;
 
